db30183060132595b985e375c7d8e5e814a307f4
MatthewGimlewicz <gimlewiczm@gmail.com>
2019-05-14 18:23:54 -0400

    added functionality to node

--- java-rrt/src/Node.java
+++ java-rrt/src/Node.java
@@ -1,4 +1,54 @@
 
 public class Node {
-
+	
+	private Node north;
+	
+	private Node south;
+	
+	private Node east;
+	
+	private Node west;
+	
+	private int id;
+	
+	public Node(int id) {
+		this.id = id;
+	}
+	
+	public Node getNorth() {
+		return north;
+	}
+	
+	public Node getSouth() {
+		return south;
+	}
+	
+	public Node getEast() {
+		return east;
+	}
+	
+	public Node getWest() {
+		return west;
+	}
+	
+	public void setNorth(Node node) {
+		north = node;
+	}
+	
+	public void setSouth(Node node) {
+		south = node;
+	}
+	
+	public void setEast(Node node) {
+		east = node;
+	}
+	
+	public void setWest(Node node) {
+		west = node;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
 }
