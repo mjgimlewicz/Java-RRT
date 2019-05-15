54bb0588f03577fa8316902457678fc8dbf6bdb9
MatthewGimlewicz <gimlewiczm@gmail.com>
2019-05-14 20:25:35 -0400

    added ImageJ lib

--- java-rrt/src/Map.java
+++ java-rrt/src/Map.java
@@ -1,19 +1,35 @@
 import java.util.ArrayList;
 
+import ij.ImageJ;
+
 public class Map {
+	
+	private int tileSize;
+	
+	private int nodeListLength;
+	
+	private Node[] nodeList;
+	
+	private ImageJ occupancyGrid;
 	
 	public ArrayList<Node> getNodeList() {
 		return null;
 	}
 	
 	public int getNodeListLength() {
-		return -1;
+		return nodeListLength;
+	}
+	
+	public ImageJ getOccupancyGrid() {
+		return occupancyGrid;
 	}
 	
 	private int generateNodeID() {
 		return -1;
 	}
 	
-	
+	private ImageJ image(ImageJ im) {
+		return null;
+	}
 	
 }
